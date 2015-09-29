ef995c32dee5386c9fa455b6d0d542347bdd5b09
Matthew Maurer <matthew.r.maurer@gmail.com>
2015-09-29 14:26:51 -0400

    Disable tests which need a postgres server to succeed

--- pkgs/holmes.nix
+++ pkgs/holmes.nix
@@ -9,6 +9,7 @@ buildRustPackage rec {
     rev   = "45e9014cc05e5f3384ff67981007c26e043db9c4";
     sha256 = "1664ss0ax88i54agdiifzfdlrw0lzzdniwdfi8jsyibasd12j8ky";
   };
+  doCheck = false;
   buildInputs = [ openssl capnproto which postgresql ];
   depsSha256 = "06mp063wk5yvf7nxxxhjys0nmklfx6i3iw5ggiss8r14iiwgg1iq";
 }
